1ebc6866071e27c7a30ced93bbe3c1566288f119
Murad Marko Pro <mpro@MacBook-Air-von-Murad.local>
2026-05-08 13:01:23 +0200

    Changed

--- src/main/java/com/greenie/chat/GreenieController.java
+++ src/main/java/com/greenie/chat/GreenieController.java
@@ -1,14 +1,33 @@
 package com.greenie.chat;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GreenieController {
 
     @GetMapping("/")
-    public String index() {
-        return "Hello World!";
+    public String helloWorld() {
+        return "Hello World";
     }
 
+    @GetMapping("/data")
+    public List<Data> getAllData() {
+        return List.of(
+                new Data("Max", "Entwickler", 28),
+                new Data("Sofia", "Designer", 32),
+                new Data("Ali", "Produktmanager", 41)
+        );
+    }
+
+    public record Data(String user, String beruf, int alter) {}
+
+    @PostMapping("/data")
+    public Data createData(@RequestBody Data data) {
+        return new Data(data.user(), data.beruf(), data.alter());
+    }
 }
